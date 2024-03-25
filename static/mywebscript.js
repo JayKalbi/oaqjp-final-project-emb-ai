0e01e036681ecf21fc8b38164a30afa20f44ffc3
Jay Kalbi <jdkalbi18@gmail.com>
2024-03-26 01:19:03 +0530

    Update mywebscript.js

--- static/mywebscript.js
+++ static/mywebscript.js
@@ -1,12 +1,17 @@
-let RunSentimentAnalysis = ()=>{
+let RunSentimentAnalysis = () => {
     textToAnalyze = document.getElementById("textToAnalyze").value;
 
     let xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            document.getElementById("system_response").innerHTML = xhttp.responseText;
+        if (this.readyState == 4) {
+            if (this.status == 200) {
+                document.getElementById("system_response").innerHTML = xhttp.responseText;
+            } else {
+                console.error("Error:", xhttp.status, xhttp.statusText);
+            }
         }
     };
-    xhttp.open("GET", "emotionDetector?textToAnalyze"+"="+textToAnalyze, true);
-    xhttp.send();
+    xhttp.open("POST", "/emotionDetector", true);
+    xhttp.setRequestHeader("Content-Type", "application/json");
+    xhttp.send(JSON.stringify({textToAnalyze: textToAnalyze}));
 }
